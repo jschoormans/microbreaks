7896a66457302af8926108875213b2e65176b8dd
jschoormans <jasper.schoormans@gmail.com>
2026-09-02 21:22:56 +0200

    Add iOS theme and system-sound helper.

---
+++ ios/MicroBreaks/Theme.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+enum MBTheme {
+    static let accent = Color(red: 13 / 255, green: 148 / 255, blue: 136 / 255) // #0d9488
+    static let accentHover = Color(red: 15 / 255, green: 118 / 255, blue: 110 / 255) // #0f766e
+    static let background = Color(red: 244 / 255, green: 246 / 255, blue: 245 / 255) // #f4f6f5
+    static let ink = Color(red: 26 / 255, green: 35 / 255, blue: 50 / 255) // #1a2332
+    static let muted = Color(red: 92 / 255, green: 107 / 255, blue: 122 / 255) // #5c6b7a
+    static let faint = Color(red: 138 / 255, green: 151 / 255, blue: 165 / 255) // #8a97a5
+    static let border = Color(red: 228 / 255, green: 233 / 255, blue: 236 / 255) // #e4e9ec
+    static let card = Color.white
+    static let soft = Color(red: 236 / 255, green: 253 / 255, blue: 248 / 255) // #ecfdf8
+}
