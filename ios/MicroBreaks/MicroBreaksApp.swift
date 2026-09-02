608904edda012226bf50c26a121a9b5a46de0199
jschoormans <jasper.schoormans@gmail.com>
2026-09-02 21:34:37 +0200

    iOS week-1: restore 25/50/custom duration on launch

--- ios/MicroBreaks/MicroBreaksApp.swift
+++ ios/MicroBreaks/MicroBreaksApp.swift
@@ -6,6 +6,8 @@ struct MicroBreaksApp: App {
     @StateObject private var store = StoreManager()
     @AppStorage("soundEnabled") private var soundEnabled = true
     @AppStorage("founderUnlocked") private var founderUnlocked = false
+    @AppStorage("durationPreset") private var durationPreset = "25"
+    @AppStorage("customMinutes") private var customMinutes = 40
 
     var body: some Scene {
         WindowGroup {
@@ -14,6 +16,7 @@ struct MicroBreaksApp: App {
                 .environmentObject(store)
                 .onAppear {
                     SoundPlayer.shared.isEnabled = soundEnabled
+                    applyDurationPreset()
                 }
                 .onChange(of: soundEnabled) { _, value in
                     SoundPlayer.shared.isEnabled = value
@@ -30,5 +33,15 @@ struct MicroBreaksApp: App {
                     }
                 }
         }
+    }
+
+    private func applyDurationPreset() {
+        let minutes: Int
+        switch durationPreset {
+        case "50": minutes = 50
+        case "custom": minutes = customMinutes
+        default: minutes = 25
+        }
+        timer.applyWorkMinutes(minutes)
     }
 }
